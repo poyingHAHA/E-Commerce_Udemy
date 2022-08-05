25eb687817df95a856879e628b36f50a0850b9e9
poyingHAHA <poying.code@gmail.com>
2022-08-05 10:34:41 +0800

    2-31 Using Include to get related Entity of the field

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -24,12 +24,18 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductByIdAsync(long id)
         {
-            return await this.context.Products.FindAsync(id);
+            return await this.context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .SingleOrDefaultAsync( p => p.Id == id);
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await this.context.Products.ToListAsync();
+            return await this.context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
