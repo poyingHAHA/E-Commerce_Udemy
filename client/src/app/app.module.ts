e345a503239f3315ab0ee530c06a9a035186b852
poyingHAHA <poying.code@gmail.com>
2022-08-07 14:16:55 +0800

    8-88 a litte modification

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 
