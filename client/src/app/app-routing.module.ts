564aaa99ff217c9517291d54f07c0e61e9f3430c
poyingHAHA <poying.code@gmail.com>
2022-08-08 07:39:54 +0800

    10-109 creating the routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'shop', component: ShopComponent},
+  {path: 'shop/:id', component: ProductDetailsComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'} // redirect to HomeComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
