d8f567effa83a9a5fd9744674ecbc0e1d760cf0e
poyingHAHA <poying.code@gmail.com>
2022-08-07 18:36:36 +0800

    9-98 Adding shareModule to shopModule. Using Pagination from ngx-bootstrap

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {PaginationModule} from 'ngx-bootstrap/pagination';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    // anything that we add to our shared module, we also need to export because we're going to be importing
+    // our shared module into any feature modules that need the functionality that we're providing inside thisshared module.
+    PaginationModule.forRoot()
+    // Now we need to add foRoot here as the pagination module has its own provider's array and
+    // those providers need to be injected into our root module at startup. And if we take off the four routes,
+    // then it won't load with its providers and will have errors.
+  ],
+  exports: [PaginationModule]
 })
 export class SharedModule { }
