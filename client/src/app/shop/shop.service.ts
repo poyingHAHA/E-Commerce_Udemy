f4f7216e414ba5569ef275fd5a7b9e1094ce104a
poyingHAHA <poying.code@gmail.com>
2022-08-07 14:38:15 +0800

    9-89 Intro to angular Service which is used to call API at the client side.

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,16 @@
+import { IPagination } from '../shared/models/pagination';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = 'https://localhost:5001/api/'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getProduct(){
+    return this.http.get<IPagination>(this.baseUrl + '/products')
+  }
 }
