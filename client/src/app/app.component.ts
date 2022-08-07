f6e83a2f40eb17cb61c8493836a3e0571bca641f
poyingHAHA <poying.code@gmail.com>
2022-08-07 14:15:41 +0800

    8-88 Orfanizing files and folder by creating core(singleton)、shared and feature(shop) module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
       next: (response: IPagination) => {
-        console.log(response);
+        // console.log(response);
         this.products = response.data;
       },
       error: error => {
@@ -25,5 +25,4 @@ export class AppComponent implements OnInit {
       }
     })
   }
-
 }
