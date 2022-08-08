6588f3b81416351dc74806e8703e718c813c85d3
poyingHAHA <poying.code@gmail.com>
2022-08-08 17:58:58 +0800

    128. Setting the section header title from the breadcrumb service

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-header.component.scss']
 })
 export class SectionHeaderComponent implements OnInit {
+  breadcrumb$: Observable<any[]>
 
-  constructor() { }
+  constructor(private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
+    // And what we also have here is an observable of the breadcrumb itself.
+    // Now, there is a general rule of thumb that if you subscribe to something, then you should always unsubscribe from it.
+    // In terms of HTTP requests in Angular and HTTP request is considered finite, It has a start and an end. And when the http request response has been completed.
+    // Then Angular itself's calls the complete unsubscribed.
+    this.breadcrumb$ = this.bcService.breadcrumbs$;
   }
 
 }
