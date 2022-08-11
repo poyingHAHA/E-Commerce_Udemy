a5e7298942a62dc803d170af29ed7693faea7caf
poyingHAHA <poying.code@gmail.com>
2022-08-11 16:54:50 +0800

    159. Hooking up the product-detail component to the basket

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -11,14 +12,35 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
   // ActivatedRoute gives us access to the routes parameters so we can get the routes that we're activating and use this information to pass it as the id of the product to our API.
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) {
-    this.bcService.set('@productDetails', ' ')
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
+  ) {
+    this.bcService.set('@productDetails', ' ');
   }
 
   ngOnInit(): void {
     this.loadProduct()
+  }
+
+  addItemToBasket(){
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity(){
+    this.quantity++;
+  }
+
+  decrementQuantity(){
+    if(this.quantity > 1){
+      this.quantity--;
+    }
+
   }
 
   loadProduct(){
