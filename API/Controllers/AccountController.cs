bd6034a846c3ff7f5bc0fae54be01b1d44aade7f
poyingHAHA <poying.code@gmail.com>
2022-08-12 08:21:00 +0800

    170. Adding login in account controller(using userManager and signInManager)

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using API.Errors;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            // In order to login in We need a combination of our userManager to go and get the user from our database, 
+            // and then we'll use our signingManager to check the user's password against what's stored in our database.
+            var user = await this.userManager.FindByEmailAsync(loginDto.Email);
+            if(user == null) return Unauthorized(new ApiResponse(401));
+
+            var result = await this.signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if(!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "This will be a token",
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}
