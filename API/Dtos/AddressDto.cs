ae0ad3ed8d1a399c8909c1d599fa567cebc54dbc
poyingHAHA <poying.code@gmail.com>
2022-08-13 16:00:21 +0800

    182. Add validation to DTO which is the good to validate against bad data, because this is where we receive from the user.

--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,18 @@ namespace API.Dtos
 {
     public class AddressDto
     {
+        // ASP NET CORE provide us with data annotations we can use, which will take care of the validation.
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Zipcode { get; set; }
     }
 }
