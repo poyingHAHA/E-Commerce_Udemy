d455747dfb61135c89f490096b48c0ee3e3e8afd
poyingHAHA <poying.code@gmail.com>
2022-08-14 20:21:33 +0800

    211. Configuration the order entities

--- Core/Entities/OrderAggregate/OrderItem.cs
+++ Core/Entities/OrderAggregate/OrderItem.cs
@@ -13,12 +13,12 @@ namespace Core.Entities.OrderAggregate
 
         public OrderItem(ProductItemOrdered itemOrdered, decimal price, int quantity)
         {
-            this.itemOrdered = itemOrdered;
+            this.ItemOrdered = itemOrdered;
             Price = price;
             Quantity = quantity;
         }
 
-        public ProductItemOrdered itemOrdered { get; set; }
+        public ProductItemOrdered ItemOrdered { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
     }
