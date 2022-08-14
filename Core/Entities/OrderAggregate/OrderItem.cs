5de7903e6e73f6295d24ad6bb5ef568a105583a4
poyingHAHA <poying.code@gmail.com>
2022-08-14 19:49:47 +0800

    210. Creating the order aggregate

---
+++ Core/Entities/OrderAggregate/OrderItem.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class OrderItem : BaseEntity
+    {
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(ProductItemOrdered itemOrdered, decimal price, int quantity)
+        {
+            this.itemOrdered = itemOrdered;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public ProductItemOrdered itemOrdered { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
