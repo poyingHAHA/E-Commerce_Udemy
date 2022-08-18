5063c70bf4aba3b3e50ee2a9fb3baad25883d2e2
poyingHAHA <poying.code@gmail.com>
2022-08-18 21:58:08 +0800

    250. Checkout success page

--- client/src/app/checkout/checkout-routing.module.ts
+++ client/src/app/checkout/checkout-routing.module.ts
@@ -2,9 +2,11 @@ import { CheckoutComponent } from './checkout.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
 
 const routes: Routes = [
-  {path: '', component: CheckoutComponent}
+  {path: '', component: CheckoutComponent},
+  {path: 'success', component: CheckoutSuccessComponent},
 ]
 
 @NgModule({
