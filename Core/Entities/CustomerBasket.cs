057dbed32909c7c0ada22c9da0111af3f66ebb24
poyingHAHA <poying.code@gmail.com>
2022-08-19 16:48:31 +0800

    261. Implementing the payment intent in the PaymentService

--- Core/Entities/CustomerBasket.cs
+++ Core/Entities/CustomerBasket.cs
@@ -18,5 +18,13 @@ namespace Core.Entities
 
         public string Id { get; set; }
         public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+        public long? DeliveryMethodId { get; set; }
+
+        // this is going to be used by Stripe so the user can confirm the payment intent.
+        public string ClientSecret { get; set; }
+        // we'll use this one to be able to update the payment intent if the client makes a change to the delivery method, to the order in any way, 
+        // they go back to the basket, they add more stuff in after they've already created a payment intent, then we're going to want to update that payment intent rather than creating a new one.
+        public string PaymentIntentId { get; set; }
+
     }
 }
--- Infrastructure/Services/PaymentService.cs
+++ Infrastructure/Services/PaymentService.cs
@@ -3,15 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Stripe;
 
 namespace Infrastructure.Services
 {
     public class PaymentService : IPaymentService
     {
-        public Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+        private readonly IBasketRepository basketRepository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IConfiguration config;
+
+        public PaymentService(IBasketRepository basketRepository, IUnitOfWork unitOfWork, IConfiguration config)
         {
-            throw new NotImplementedException();
+            this.basketRepository = basketRepository;
+            this.unitOfWork = unitOfWork;
+            this.config = config;
+        }
+
+        public async Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            StripeConfiguration.ApiKey = this.config["StripeSettings:SecretKey"];
+
+            var basket = await this.basketRepository.GetBasketAsync(basketId);
+            var shippingPrice = 0m;
+
+            if(basket.DeliveryMethodId.HasValue)
+            {
+                var deliveryMethod = await this.unitOfWork.Repository<DeliveryMethod>().GetByIdAsync((long)basket.DeliveryMethodId);
+                shippingPrice = deliveryMethod.Price;
+            } 
+
+            // check the item price in the basket
+            foreach(var item in basket.Items)
+            {
+                var productItem = await this.unitOfWork.Repository<Core.Entities.Product>().GetByIdAsync(item.Id);
+                if(item.Price != productItem.Price)
+                {
+                    item.Price = productItem.Price;
+                }
+            }
+
+            var service = new PaymentIntentService();
+
+            PaymentIntent intent;
+            if(string.IsNullOrEmpty(basket.PaymentIntentId))
+            {
+                var options = new PaymentIntentCreateOptions
+                {
+                    // And slightly more complex because stripe doesn't take decimals for the amount it takes the number in a long
+                    // in order to convert a decimal into a long, then we're going to need to multiply it by a hundred
+                    Amount = (long) basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long) shippingPrice * 100,
+                    Currency = "usd",
+                    PaymentMethodTypes = new List<string> {"card"}
+                };
+
+                intent = await service.CreateAsync(options);
+                basket.PaymentIntentId = intent.Id;
+                basket.ClientSecret = intent.ClientSecret;
+            }
+            else
+            { // this takes care of the possibility that the client's been away. They've changed to basket's after going through the check out and then backing out after we've already created a payment intent.
+                var options = new PaymentIntentUpdateOptions
+                {
+                    Amount = (long) basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long) shippingPrice * 100,
+                };
+                await service.UpdateAsync(basket.PaymentIntentId, options);
+            }
+
+            await this.basketRepository.UpdateBasketAsync(basket);
+
+            return basket;
         }
     }
 }
