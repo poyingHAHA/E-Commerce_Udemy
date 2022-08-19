34a72b7d231b72c62af7fb688b102074bd93912e
poyingHAHA <poying.code@gmail.com>
2022-08-19 14:35:09 +0800

    252~255. Client - Order Module and component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
   {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
   {path: 'basket', canActivate: [AuthGuard], loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
+  {path: 'orders', canActivate: [AuthGuard], loadChildren: () => import('./orders/orders.module').then(mod => mod.OrdersModule), data: {breadcrumb: 'Orders'}},
   {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'}},
   {path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule), data: {breadcrumb: {skip: true}}},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
---
+++ client/src/app/orders/orders.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { IOrder } from '../shared/models/order';
+import { OrdersService } from './orders.service';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss']
+})
+export class OrdersComponent implements OnInit {
+  orders: IOrder[];
+
+  constructor(private orderService: OrdersService) { }
+
+  ngOnInit(): void {
+    console.log("Orders... ")
+    this.orderService.getOrdersForUser().subscribe({
+      next: (orders: IOrder[]) => {
+        this.orders = orders;
+      },
+      error: error => {
+        console.log(error);
+      }
+    })
+  }
+
+}
