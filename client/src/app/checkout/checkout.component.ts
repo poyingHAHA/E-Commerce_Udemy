155ad36dabad5e0ee73a399157a80edd3e88722a
poyingHAHA <poying.code@gmail.com>
2022-08-20 09:12:47 +0800

    267. Adding stripe elements to the components

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -57,7 +57,6 @@ export class CheckoutComponent implements OnInit {
 
   getDeliveryMethodValue(){
     const basket = this.basketService.getCurrentBasketValue();
-    console.log(basket);
     if(basket.deliveryMethodId !== null){
       this.checkoutForm.get('deliveryForm').get('deliveryMethod').patchValue(basket.deliveryMethodId.toString());
     }
